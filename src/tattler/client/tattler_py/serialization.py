b1797ebdd5746e97ec6ef43b326878359fe78c24
mij <mij@sshguard.net>
2024-11-25 16:03:56 +0100

    support serializing mapping-like objects other than dict

--- src/tattler/client/tattler_py/serialization.py
+++ src/tattler/client/tattler_py/serialization.py
@@ -1,4 +1,8 @@
+"""Serialization logic to transport client objects faithfully over JSON to server"""
+
 from datetime import datetime, date
+
+from collections.abc import Mapping, Set
 
 import json
 
@@ -7,8 +11,10 @@ class JSONComplEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime) or isinstance(obj, date):
             return obj.isoformat()
-        if isinstance(obj, set):
-            return sorted(obj)
+        if isinstance(obj, Mapping):    # e.g. SQLAlchemy Row._mapping
+            return dict(obj)
+        if isinstance(obj, Set):
+            return sorted(obj)        
 
 def serialize_json(dictionary):
     """Serialize data into JSON supporting objects otherwise not json-serializable"""
