ca017dc8027a1960748057f48cf83ddb9108692d
mij <mij@sshguard.net>
2026-03-30 08:33:26 +0200

    update copyright year to 2026

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'tattler'
-copyright = '2023 - 2024, tattler.dev'
+copyright = '2023 - 2026, tattler.dev'
 author = 'Michele Mazzucchi, keencons.com'
 release = '3.0.0'
 
